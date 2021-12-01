b719ab072a9e8e7e0c2de138e5b8e8f503670a0b
Everton Frozza <evertonfrozza@gmail.com>
2021-12-01 19:42:49 -0300

    inicial

---
+++ src/main/java/br/com/efrozza/awsprojeto01/AwsProjeto01Application.java
@@ -0,0 +1,13 @@
+package br.com.efrozza.awsprojeto01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjeto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjeto01Application.class, args);
+	}
+
+}
