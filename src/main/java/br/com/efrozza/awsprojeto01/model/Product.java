1f3ae759a4ae35deb999dc58220cf1fe1b982e98
Everton Frozza <evertonfrozza@gmail.com>
2022-01-04 19:46:50 -0300

    criacao do model Product

---
+++ src/main/java/br/com/efrozza/awsprojeto01/model/Product.java
@@ -0,0 +1,37 @@
+package br.com.efrozza.awsprojeto01.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+// garantir unicidade por algum campo da tabela
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"code"})
+        }
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(length = 32, nullable = false)
+    private String name;
+
+    @Column(length = 24, nullable = false)
+    private String model;
+
+    @Column(length = 8, nullable = false)
+    private String code;
+
+    private float price;
+
+
+
+
+}
