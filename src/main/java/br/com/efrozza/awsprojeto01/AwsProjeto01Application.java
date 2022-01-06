647ed5528a6a4f97ab165b73e5bbaef488f7fa0c
Everton Frozza <evertonfrozza@gmail.com>
2022-01-06 07:55:22 -0300

    ajuste properties db

--- src/main/java/br/com/efrozza/awsprojeto01/AwsProjeto01Application.java
+++ src/main/java/br/com/efrozza/awsprojeto01/AwsProjeto01Application.java
@@ -2,14 +2,12 @@ package br.com.efrozza.awsprojeto01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = "br.com.efrozza.awsprojeto01.model")
 @SpringBootApplication
 public class AwsProjeto01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsProjeto01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AwsProjeto01Application.class, args);
+    }
 
 }
