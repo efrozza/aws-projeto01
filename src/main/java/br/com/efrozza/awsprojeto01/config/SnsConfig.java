42d0e84a3adbdb20a8781841a7bfa190bcc70e4d
Everton Frozza <evertonfrozza@gmail.com>
2022-01-11 20:09:16 -0300

    ajuste lib aws sns e criacao da classe de config

--- src/main/java/br/com/efrozza/awsprojeto01/config/SnsConfig.java
+++ src/main/java/br/com/efrozza/awsprojeto01/config/SnsConfig.java
@@ -1,6 +1,11 @@
 package br.com.efrozza.awsprojeto01.config;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,5 +16,18 @@ public class SnsConfig {
 
     @Value("${aws.sns.topic.product.events.arn}")
     private String productEventTopic;
+
+    @Bean
+    public AmazonSNS snsClient(){
+        return AmazonSNSAsyncClientBuilder.standard()
+                .withRegion(awsRegion)
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+
+    @Bean(name = "productEventTopic")
+    public Topic snsProductEventTopic(){
+        return new Topic().withTopicArn(productEventTopic);
+    }
 
 }
